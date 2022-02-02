da747c8ae5608b03842edcda6d0acd25d18f2126
Alexey Deshchenia <d13laersim@openmygame.com>
2022-02-02 22:52:53 +0300

    TRAJECTORIES COUNTING
    1. Made the fruits become unactive when they are out of scene bounds
    2. Set the top-limit for the fruits jumps
    3. Created some GD tools in the editor

--- Assets/Scripts/Controllers/FruitsMover.cs
+++ Assets/Scripts/Controllers/FruitsMover.cs
@@ -1,77 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
+using Models;
 using UnityEngine;
 
-public class FruitsMover : MonoBehaviour
+namespace Controllers
 {
-    [Range(0, 200)] [SerializeField] private float fruitStartSpeed = 100;
-    [Range(0, 2)] [SerializeField] private float gravity = 1;
-    [Range(0, 10)] [SerializeField] private float jumpDuration = 1;
-
-    [SerializeField] private RectTransform screenRectTransform;
-
-    [SerializeField] private GameObject fruitPrefab;
-    [SerializeField] private GameObject pointPrefab;
-
-    private Vector2 _screenSize;
-    private Vector3 _bottomLeftPoint;
-    private Vector3 _bottomRightPoint;
-    private Vector3 _topLeftPoint;
-    private Vector3 _topRightPoint;
-    private float _yCeiling;
-
-    private void Start()
+    public class FruitsMover : MonoBehaviour
     {
-        _screenSize = screenRectTransform.rect.size;
-        _yCeiling = _screenSize.y / 2;
-        _bottomLeftPoint = new Vector3(-_screenSize.x / 2, -_yCeiling);
-        _bottomRightPoint = new Vector3(_screenSize.x / 2, -_yCeiling);
-        _topLeftPoint = new Vector3(-_screenSize.x / 2, _yCeiling);
-        _topRightPoint = new Vector3(-_screenSize.x / 2, _yCeiling);
-        
-        var fruit = Instantiate(fruitPrefab, transform);
-        var fruitRt = fruit.GetComponent<RectTransform>();
-        fruit.transform.localPosition = _bottomLeftPoint;
-        var positions = TrajectoryCounter.GetPoints(fruit.transform.localPosition, fruitStartSpeed, 1, 80, gravity);
+        [Range(0, 200)] [SerializeField] private float fruitStartSpeed = 100;
+        [Range(0, 2)] [SerializeField] private float gravity = 1;
+        [Range(0, 10)] [SerializeField] private float jumpDuration = 1;
+        [Range(0, 1)] [SerializeField] private float bottomSpawnProbability = 0.8f;
+        [Range(0, 1)] [SerializeField] private float sideSpawnProbability = 0.1f;
+        [Range(1, 10)] [SerializeField] private int numberOfFruits = 4;
 
-        var maxY = GetTheTopOfTrajectory(positions);
-        if (maxY >= _yCeiling - fruitRt.rect.height)
+        [SerializeField] private RectTransform screenRectTransform;
+
+        [SerializeField] private Transform poolTransform;
+        [SerializeField] private GameObject fruitPrefab;
+        [SerializeField] private GameObject pointPrefab;
+
+        private Vector2 _screenSize;
+        private Vector3 _bottomLeftPoint;
+        private Vector3 _bottomRightPoint;
+        private Vector3 _topLeftPoint;
+        private Vector3 _topRightPoint;
+        private float _yCeiling;
+
+        private TrajectoryCounter _trajectoryCounter;
+
+        private void Awake()
         {
-            var delta = (maxY + _yCeiling / 2) / (_yCeiling * 2);
-            positions = TrajectoryCounter.GetPoints(fruit.transform.localPosition, fruitStartSpeed, 1, 60, gravity * delta);
+            _trajectoryCounter = new TrajectoryCounter();
         }
-        
-        DOTween.Sequence()
-            .Append(fruitRt.DOLocalPath(positions, jumpDuration).SetEase(Ease.InOutQuad));
 
-        ShowTestTrajectory(positions);
-    }
-
-    private Vector3 GetRandomStartPoint(float fruitHeight)
-    {
-        
-
-        var randomX = Random.Range(_bottomLeftPoint.x, _bottomRightPoint.x);
-        var randomY = -_screenSize.y / 2;
-        
-        return new Vector3(0, -_screenSize.y / 2 - fruitHeight);
-    }
-
-    private float GetTheTopOfTrajectory(Vector3[] trajectory) => trajectory[trajectory.Length / 2].y;
-
-    private void ShowTestTrajectory(Vector3[] positions)
-    {
-        int ind = 0;
-        foreach (var pos in positions)
+        private void Start()
         {
-            if (ind % 10 == 0)
-            {
-                var point = Instantiate(pointPrefab, transform);
-                point.transform.localPosition = pos;
-            }
+            _screenSize = screenRectTransform.rect.size;
+            _yCeiling = _screenSize.y / 2;
+            _bottomLeftPoint = new Vector3(-_screenSize.x / 2, -_yCeiling);
+            _bottomRightPoint = new Vector3(_screenSize.x / 2, -_yCeiling);
+            _topLeftPoint = new Vector3(-_screenSize.x / 2, _yCeiling);
+            _topRightPoint = new Vector3(-_screenSize.x / 2, _yCeiling);
 
-            ind++;
+            LaunchFruits();
+        }
+
+        private void LaunchFruits()
+        {
+            var delay = 0f;
+            for (var i = 0; i < numberOfFruits; i++)
+            {
+                delay += Random.Range(0f, 1f);
+                var fruit = Instantiate(fruitPrefab, poolTransform);
+                var fruitRt = fruit.GetComponent<RectTransform>();
+                var fruitHeight = fruitRt.rect.height;
+                var fruitRadius = fruitHeight / 2;
+                var (startPoint, angle) = GetRandomStartValues(fruitHeight);
+                var positions = _trajectoryCounter.GetPoints(startPoint, fruitStartSpeed, 1, angle, gravity, _screenSize, fruitRadius);
+                fruit.transform.localPosition = positions[0];
+                DOTween.Sequence()
+                    .AppendInterval(delay)
+                    .Append(fruitRt.DOLocalPath(positions, jumpDuration).SetEase(Ease.InOutQuad))
+                    .AppendCallback(() => { fruit.SetActive(false); });
+            }
+        }
+
+        private (Vector3 startPoint, float angle) GetRandomStartValues(float fruitHeight)
+        {
+            Vector3 startPoint;
+            float angle;
+            float spawnValidator = Random.Range(0f, 1f);
+        
+            if (spawnValidator <= sideSpawnProbability)
+            {
+                startPoint = new Vector3(- fruitHeight - _screenSize.x / 2, Random.Range(-_yCeiling, _yCeiling / 2));
+                angle = Random.Range(35f, 85f);
+            }
+            else if (spawnValidator <= bottomSpawnProbability)
+            {
+                startPoint = new Vector3(Random.Range(-_screenSize.x / 2, 0), - fruitHeight -_yCeiling);
+                angle = Random.Range(35f, 85f);
+            }
+            else
+            {
+                startPoint = new Vector3(fruitHeight + _screenSize.x / 2, Random.Range(-_yCeiling, _yCeiling / 2));
+                angle = Random.Range(95f, 135f);
+            }
+            
+            return (startPoint, angle);
         }
     }
 }
--- Assets/Scripts/Models/TrajectoryCounter.cs
+++ Assets/Scripts/Models/TrajectoryCounter.cs
@@ -2,30 +2,63 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class TrajectoryCounter
+namespace Models
 {
-    private const float G = 9.81f;
-    public static Vector3[] GetPoints(Vector3 startPosition, float speed, float timeSectionLength, float horizonAngle, float gravity)
+    public class TrajectoryCounter : ClassSingleton<TrajectoryCounter>
     {
-        var pointsList = new List<Vector3>();
-        var timeSection = 0f;
-        var yPos = 0f;
-        horizonAngle *= Mathf.Deg2Rad;
-        var angleTan = (float)Math.Tan(horizonAngle);
-        var angleCos = (float)Math.Cos(horizonAngle);
-        var angleCosPow = (float) Math.Pow(angleCos, 2);
-        var speedPow = (float) Math.Pow(speed, 2);
-        
-        while (yPos + startPosition.y >= startPosition.y)
+        private const float G = 9.81f;
+        public Vector3[] GetPoints(Vector3 startPosition, float speed, float timeSectionLength, float horizonAngle, float gravity, Vector2 sceneSize, float fruitRadius)
         {
-            var xPos = timeSection * angleCos;
-            var gravityPower = xPos * G * gravity / (2 * speedPow * angleCosPow);
-            yPos = xPos * (angleTan - gravityPower);
+            var pointsList = new List<Vector3>();
+            var timeSection = 1f;
+            var yPos = 0f;
+            horizonAngle *= Mathf.Deg2Rad;
+            var angleTan = (float)Math.Tan(horizonAngle);
+            var angleCos = (float)Math.Cos(horizonAngle);
+            var angleCosPow = (float) Math.Pow(angleCos, 2);
+            var speedPow = (float) Math.Pow(speed, 2);
+            
+            var tempY = startPosition.y;
+            while (yPos + startPosition.y >= - sceneSize.y / 2 - fruitRadius || yPos + startPosition.y >= startPosition.y)
+            {
+                var xPos = timeSection * angleCos;
+                var gravityPower = xPos * G * gravity / (2 * speedPow * angleCosPow);
+                yPos = xPos * (angleTan - gravityPower);
+                timeSection += timeSectionLength;
+                
+                if (yPos + startPosition.y + fruitRadius > sceneSize.y / 2.5f && tempY > yPos)
+                {
+                    var delta = (yPos + startPosition.y) - sceneSize.y / 2.5f;
+                    startPosition -= new Vector3(0, delta + fruitRadius);
+                    break;
+                }
 
-            pointsList.Add(startPosition + new Vector3(xPos, yPos));
-            timeSection += timeSectionLength;
-        }
+                tempY = yPos;
+            }
+
+            timeSection = 1f;
+            Vector3 point = default;
         
-        return pointsList.ToArray();
+            while (point.y >= startPosition.y || point.y > - sceneSize.y / 2 - fruitRadius)
+            {
+                var xPos = timeSection * angleCos;
+                var gravityPower = xPos * G * gravity / (2 * speedPow * angleCosPow);
+                yPos = xPos * (angleTan - gravityPower);
+                point = startPosition + new Vector3(xPos, yPos);
+                if (CheckIfPointOnScene(point, sceneSize, fruitRadius))
+                    pointsList.Add(point);
+                timeSection += timeSectionLength;
+            }
+        
+            return pointsList.ToArray();
+        }
+
+        private bool CheckIfPointOnScene(Vector3 point, Vector2 sceneSize, float indent)
+        {
+            return point.x > - sceneSize.x / 2 - indent
+                   && point.x < sceneSize.x / 2 + indent
+                   && point.y > - sceneSize.y / 2 - indent
+                   && point.y < sceneSize.y / 2 + indent;
+        }
     }
 }
